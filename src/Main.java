71025c3e322043a397a7672c97fd18fdb956f933
Christian Wong <dev@christianwong.com>
2015-02-16 18:51:56 -0200

    updated main object name #dummy

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Object object = Tester.run(FILENAME);
-		TypeFactory.getType("OBJECT", object);
+		TypeFactory.getType("bin_object", object);
 	}
 
 }
