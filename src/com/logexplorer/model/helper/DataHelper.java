2179fd79b952ff2238829ddcab5a5ef230c52ab3
Christian Wong <dev@christianwong.com>
2015-02-27 23:00:16 -0300

    added support for AbstractType in DataHelper

--- src/com/logexplorer/model/helper/DataHelper.java
+++ src/com/logexplorer/model/helper/DataHelper.java
@@ -3,6 +3,8 @@ package com.logexplorer.model.helper;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.logexplorer.model.types.AbstractType;
+
 /**
  * Singleton class for storing object references and duplication detection.
  * @author christian wong
@@ -24,18 +26,17 @@ public class DataHelper {
 	}
 	
 	protected List<Integer> list;
-	protected List<Object> objects;
+	// TODO store reference to object and AbstractType in the list.
 	
 	private DataHelper() {
 		list = new ArrayList<Integer>();
-		objects = new ArrayList<Object>();
 	};
 	
 	private int getObjectHash(Object object) {
 		return System.identityHashCode(object);
 	}
 	
-	private synchronized String processObject(Object object) {
+	private synchronized String processObject(Object object, AbstractType type) {
 		int objectHash = getObjectHash(object);
 		String objectID = Integer.toString(list.size());
 		
@@ -44,15 +45,14 @@ public class DataHelper {
 			objectID = Integer.toString(indexOf)+"D";
 		} else {
 			list.add(objectHash);
-			objects.add(object);
 		}
 
 		System.out.println("object Hash: '"+objectHash+"' was assigned to objectID: '"+objectID+"'.. Object is: '"+object.toString()+"'.");
 		return objectID;
 	}
 	
-	public static String getObjectID(Object object) {
-		return DataHelper.getInstance().processObject(object);
+	public static String getObjectID(Object object, AbstractType type) {
+		return DataHelper.getInstance().processObject(object, type);
 	}
 
 //	public String getObjectID(Object object) {
