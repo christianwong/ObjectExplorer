eabd263e0a84db05decd3953a42955a445fc8ad2
Christian Wong <dev@christianwong.com>
2015-03-07 18:33:01 -0300

    sorting object fields

--- model/src/com/logexplorer/model/types/ObjectType.java
+++ model/src/com/logexplorer/model/types/ObjectType.java
@@ -4,6 +4,8 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import com.logexplorer.model.util.TypeUtils;
@@ -51,6 +53,14 @@ public class ObjectType extends AbstractType {
 				fieldList.remove(idx);
 			}
 		}
+		
+		// Sort list
+		Collections.sort(fieldList, new Comparator<Field>() {
+			@Override
+			public int compare(Field field1, Field field2) {
+				return field1.getName().compareTo(field2.getName());
+			}
+		});
 
 		return fieldList;
 	}
